299d2be1d47761d5cdf305b7046d502eae1c8eef
kfircharki123 <102507783+kfircharki123@users.noreply.github.com>
2022-03-28 14:36:33 +0300

    Update MainMenuState.hx
    1

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -36,7 +36,6 @@ class MainMenuState extends MusicBeatState
 		'story_mode',
 		'freeplay',
 		#if MODS_ALLOWED 'mods', #end
-		#if ACHIEVEMENTS_ALLOWED 'awards', #end
 		'credits',
 		#if !switch 'donate', #end
 		'options'
@@ -114,6 +113,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
+                        menuitem.scale.set(0.8, 0.8);
 			menuItem.ID = i;
 			menuItem.screenCenter(X);
 			menuItems.add(menuItem);
@@ -267,6 +267,7 @@ class MainMenuState extends MusicBeatState
 		menuItems.forEach(function(spr:FlxSprite)
 		{
 			spr.screenCenter(X);
+                        spr.x += -300;
 		});
 	}
 
